1e0382bd4be55d35997a2c724523148579ef85c6
Georgy Dzutsev <dzgeorgy@icloud.com>
2025-11-16 12:59:18 +0300

    chore: initialize go backend project (#10)
    - refactor: restructure module and add Heimly entry point with graceful shutdown

---
+++ cmd/heimly/fancy_printer.go
@@ -0,0 +1,13 @@
+package main
+
+func printWelcome() {
+	println("==========================")
+	println("   Welcome to Heimly! 👋  ")
+	println("==========================")
+}
+
+func printGoodbye() {
+	println("==========================")
+	println(" Shutting down Heimly! 👋 ")
+	println("==========================")
+}
---
+++ cmd/heimly/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+
+	printWelcome()
+
+	<-signalChan
+	printGoodbye()
+}
