b45a8edc2cbc75bb0861a2b2033524d93dc781cc
Georgy Dzutsev <dzgeorgy@icloud.com>
2026-01-08 14:50:48 +0300

    feat(cfg): implement config loading and summary printing

--- cmd/heimly/main.go
+++ cmd/heimly/main.go
@@ -4,6 +4,8 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+
+	"heimly.space/backend/internal/cfg"
 )
 
 func main() {
@@ -11,6 +13,10 @@ func main() {
 	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
 
 	printWelcome()
+
+	println("📝 Loading Heimly config...")
+	conf := cfg.Load()
+	conf.PrintSummary()
 
 	<-signalChan
 	printGoodbye()
