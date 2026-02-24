0994218ec9ebe39afbe678dea2c72662deb20064
Georgy Dzutsev <dzgeorgy@icloud.com>
2026-02-24 16:32:56 +0300

    fix(app): make graceful shutdown reliable on first stop signal

--- cmd/heimly/main.go
+++ cmd/heimly/main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"context"
 	"errors"
+	"fmt"
+	"log"
 	http2 "net/http"
-	"os"
 	"os/signal"
 	"syscall"
 	"time"
@@ -14,8 +15,8 @@ import (
 )
 
 func main() {
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
 
 	printWelcome()
 
@@ -24,20 +25,56 @@ func main() {
 	conf.PrintSummary()
 
 	application := app.New(conf)
-	defer application.DB.Close()
 
+	serverErrCh := make(chan error, 1)
 	go func() {
-		if err := application.HTTPServer.ListenAndServe(); err != nil &&
-			!errors.Is(err, http2.ErrServerClosed) {
-			panic(err)
-		}
+		serverErrCh <- application.HTTPServer.ListenAndServe()
 	}()
 
-	<-signalChan
+	select {
+	case <-ctx.Done():
+		// Allow a repeated signal (Ctrl+C / IDE stop) to force-kill immediately.
+		stop()
+		fmt.Println("🛑 Shutdown signal received")
+	case err := <-serverErrCh:
+		if err != nil && !errors.Is(err, http2.ErrServerClosed) {
+			log.Fatalf("http server failed: %v", err)
+		}
+		closeDBWithTimeout(application.DB, 3*time.Second)
+		printGoodbye()
+		return
+	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	_ = application.HTTPServer.Shutdown(ctx)
+	if err := application.HTTPServer.Shutdown(shutdownCtx); err != nil {
+		log.Printf("graceful shutdown failed: %v; forcing close", err)
+		_ = application.HTTPServer.Close()
+	}
 
+	select {
+	case err := <-serverErrCh:
+		if err != nil && !errors.Is(err, http2.ErrServerClosed) {
+			log.Printf("http server exit error: %v", err)
+		}
+	case <-time.After(2 * time.Second):
+		log.Printf("http server did not report shutdown in time; exiting")
+	}
+
+	closeDBWithTimeout(application.DB, 3*time.Second)
 	printGoodbye()
+}
+
+func closeDBWithTimeout(dbCloser interface{ Close() }, timeout time.Duration) {
+	done := make(chan struct{})
+	go func() {
+		dbCloser.Close()
+		close(done)
+	}()
+
+	select {
+	case <-done:
+	case <-time.After(timeout):
+		log.Printf("db close timed out after %s; exiting", timeout)
+	}
 }
